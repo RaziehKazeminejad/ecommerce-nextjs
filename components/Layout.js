88099b24f0494b698f15433388c493981bad7066
Razieh Kazeminejad <r.kazeminejad98@gmail.com>
2021-12-20 11:00:54 +0330

    Add Styles To Layout and Fix SSR MaterialUI Styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,23 @@
 import React, { Children } from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from '../utils/style';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Raika</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Raika</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
-          <Typography>
-              All rights reserved. Next Raika
-          </Typography>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Raika</Typography>
       </footer>
     </div>
   );
